2a8b4441904db42123408acbbf845235a8cc189b
Cornelia Dyana <corneliadyana@gmail.com>
2018-03-21 22:47:50 +0700

    Create advanced-select.sql

---
+++ mysql/advanced-select.sql
@@ -0,0 +1,21 @@
+/* not completed */
+
+/* type of triangle */
+SELECT 
+CASE 
+WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+WHEN A = B AND B = C THEN 'Equilateral'
+WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+ELSE 'Scalene'
+END
+FROM TRIANGLES;
+
+/* binary tree nodes */
+SELECT N,
+CASE
+WHEN P is NULL THEN 'Root'
+WHEN N in (SELECT P FROM BST) THEN 'Inner'
+ELSE 'Leaf'
+END
+FROM BST
+ORDER by N;
